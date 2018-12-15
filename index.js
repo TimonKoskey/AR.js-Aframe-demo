56794ef05bdb2751e1ed426820fdc348489c1638
Timon Kosy <timonkosy92@gmail.com>
2018-12-15 09:23:49 +0300

    A simple augmented reality thank you card made using aframe and ar.js

--- index.js
+++ index.js
@@ -1,30 +1,18 @@
-/*jshint esversion: 6 */
-/*jshint browser: true */
-/*jshint devel: true */
+const $ = (element_id) => document.getElementById(element_id);
 
-const $ = (query) => document.querySelector(query);
+const shiftColors = (value) => (value + 1) % 360;
 
-const sphere = $('a-sphere');
-const plane = $('a-plane');
-
-const shiftDegrees = (value) => (value + 1) % 360;
-
-let degrees = 0;
+let colors = 0;
 
 const animate = () => {
-  degrees = shiftDegrees(degrees);
-  const color = `hsl(${degrees}, 100%, 50%)`;
-  const variation = Math.sin(Date.now() / 1000);
-  const position = `0 ${1.5 + variation} -2`;
-  const rotation = `-90 0 ${degrees}`;
+  colors = shiftColors(colors);
+  const color = `hsl(${colors}, 100%, 50%)`;
+  // const variation = Math.sin(Date.now() / 100);
 
-  sphere.setAttribute('color', color);
-  sphere.setAttribute('position', position);
-
-  plane.setAttribute('color', color);
-  plane.setAttribute('rotation', rotation);
+  $('change-color').setAttribute('color', color);
 
   requestAnimationFrame(animate);
 };
 
 requestAnimationFrame(animate);
+
